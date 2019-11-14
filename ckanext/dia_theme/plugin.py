a2fc9e708d99ed56ef4d77a86589dbe1f372734c
Kevin <gitlab@sotomail.net>
2019-11-14 16:49:39 +0100

    Move theme helpers to OPERANDUM plugin

--- ckanext/dia_theme/plugin.py
+++ ckanext/dia_theme/plugin.py
@@ -1,12 +1,9 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-from . import helpers
-
 
 class Dia_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -14,10 +11,3 @@ class Dia_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'dia_theme')
-
-    def get_helpers(self):
-        return {
-            'parent_site_url': helpers.parent_site_url,
-            'modify_geojson': helpers.modify_geojson,
-            'make_nav_links': helpers.make_nav_links,
-        }
